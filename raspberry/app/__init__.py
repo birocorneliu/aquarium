6f04e6d72a3fa4653d741d9461d7fc6f978f2036
Corneliu Biro <corneliu.biro@gmail.com>
2016-02-03 09:27:33 +0200

    changes

--- raspberry/app/__init__.py
+++ raspberry/app/__init__.py
@@ -5,17 +5,17 @@ ROUTES = (
     (["GET"], "/reload_pins", reload_pins),
     (["GET"], "/doser/<pin_id>/quantity/<int:quantity>", doser),
     (["GET"], "/temperature", temperature),
-    (["GET"], "/get_temperatures", get_temperatures),
-    (["GET", "POST"], "/temperature", temperature),
     (["GET"], "/pin/<pin_id>/open", open_pin),
     (["GET"], "/pin/<pin_id>/close", close_pin),
     (["GET"], "/set_times_to_cron", set_times_to_cron),
     (["GET"], "/set_procedure/<procedure>", set_procedure),
+
+    (["GET"], "/get_temperatures", get_temperatures),
+    (["GET", "POST"], "/temperature", temperature),
 )
 
 
-from app.cron import Cron
-
+#from app.cron import Cron
 #Set CronTab
 #Cron.clean_cron()
 #Cron.set_times()
