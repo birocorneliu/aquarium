340f81d3b65e1e54eb7fc66dcd688a69661787bc
Corneliu Biro <corneliu.biro@gmail.com>
2019-02-13 01:47:16 +0200

    Add basic client app
    Signed-off-by: Corneliu Biro <corneliu.biro@gmail.com>

--- raspberry/server.py
+++ raspberry/server.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from flask import Flask, redirect
+from flask import Flask, redirect, send_from_directory
 try:
     from flask.ext.cors import CORS
 except:
@@ -10,16 +10,30 @@ from app import ROUTES
 from lib.exceptions import APPException
 
 
+
+
 #Start app engine
 app = Flask(__name__)
 app.secret_key = "Super secret KEY"
 CORS(app)
 
 
+# Add client files
+client_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'client')
+
+@app.route('/client', methods=['GET'])
+def serve_dir_directory_index():
+    return send_from_directory(client_file_dir, 'index.html')
+
+
+@app.route('/client/<path:path>', methods=['GET'])
+def serve_file_in_dir(path):
+    return send_from_directory(client_file_dir, path)
+
+
 #Add rules to the app
 for methods, path, func in ROUTES:
     app.add_url_rule(path, view_func=func, methods=methods)
-
 
 #Add exception handler
 @app.errorhandler(APPException)
@@ -39,5 +53,4 @@ sys.path.append(PATH)
 if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0', port=80)
-
 
